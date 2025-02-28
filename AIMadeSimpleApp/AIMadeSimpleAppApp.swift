2c90aa4ad6e71a47ec37512b8e190c430317b12d
Denis Kravets <krde0503@gmail.com>
2025-02-28 14:50:20 +0100

    - add(project): добавил проект

---
+++ AIMadeSimpleApp/AIMadeSimpleAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  AIMadeSimpleAppApp.swift
+//  AIMadeSimpleApp
+//
+//  Created by Denis Kravets on 28.2.25.
+//  Copyright © 2025 ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct AIMadeSimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
